794a76f404db859f62d657119d785b9b19154e40
FRANCIS\franc <ciscoallen12@gmail.com>
2023-12-20 15:52:56 -0600

    cambio del navbar

--- index.js
+++ index.js
@@ -37,6 +37,20 @@ function startAnimation(entries, observer) {
     observer.observe(textDisplay)
   },6000)
 
+  let hamburger=document.querySelector('.hamburger')
+  let navlinks=document.getElementById('nav-list')
+  let links=document.querySelectorAll('.links')
+
+  hamburger.addEventListener('click',()=>{
+    navlinks.classList.toggle('hide');
+    hamburger.classList.toggle('lines-rotate')
+  })
+
+  for(let i=0;i<links.length;i++){
+    links[i].addEventListener('click',()=>{
+      navlinks.classList.add('hide');
+    })
+  }
 
 
 
